17ccb772314fff5a15144046720a0aabf55e6e9a
jeisonrosario66 <96961824+jeisonrosario66@users.noreply.github.com>
2023-05-05 13:43:34 -0400

    main.py-update-objeto creado

--- main.py
+++ main.py
@@ -1,95 +1,30 @@
-#import   
 import openai
 from env import ORG, APIKEY
 
-openai.organization = ORG
-openai.api_key = APIKEY
-#openai.Model.list()
-
-conversation = ""
 print("\t\texit() para salir")
 
-def botChat():
-    while True:
-        question = input("Humano: ")
-        if question == "exit()":
-            break
-        else:
-            conversation += "\nHumano: " + question + "\nAi:"
+class BotChat():
+    def __init__(self, userName):
+        openai.organization = ORG
+        openai.api_key = APIKEY
+        self.userName = userName
+        self.nameAI = "AI"
+    
+    def pregunta(self):
+        while True:
+            conversacion = ""
+            question = input(f"{self.userName}: ")
+            conversacion += f"\n{self.userName}: " + question + f"\n{self.nameAI}:"
             response = openai.Completion.create(
                 engine="davinci",
-                prompt=conversation,
+                prompt=conversacion,
                 temperature=0.9,
                 max_tokens=250,
                 top_p=1,
                 frequency_penalty=0,
                 presence_penalty=0.6,
-                stop=["\n", " Humano:", " AI:"]
+                stop=["\n", f" {self.userName}:", f" {self.nameAI}:"]
             )
             anwer=response.choices[0].text.strip()
-            conversation += anwer 
+            conversacion += anwer 
             print("AI: " + anwer + "\n")
-
-botChat()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-""" 
-#import   
-import openai
-
-openai.api_key = "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"
-
-conversation = ""
-
-i=1
-while (i != 0):
-    question = input("Humano: ")
-    conversation += "\nHumano: " + question + "\nAi:"
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=conversation,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " Humano:", " AI:"]
-    )
-    anwer=response.choices[0].text.strip()
-    conversation += anwer 
-    print("AI: " + anwer + "\n")
-"""    
